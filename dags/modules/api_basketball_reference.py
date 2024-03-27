e7461895e3f213b69e3c7423816a4a5a4595ca15
Achmad Rifki <74856462+achmadrrb@users.noreply.github.com>
2024-03-27 15:22:10 +0700

    Feature/data scraping (#1)
    * feat: delete player_stats function and unnecessary library
    
    * feat: add _parse_date_now function
    
    * feat: add _convert_date_to_str function
    
    * feat: add _get_box_score_list function

--- dags/modules/api_basketball_reference.py
+++ dags/modules/api_basketball_reference.py
@@ -6,35 +6,80 @@ Basketball Reference website: https://www.basketball-reference.com/
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning) # stop getting Pandas FutureWarning's
 
-import os
-import shutil
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime
+import pytz
 
-driver = webdriver.Chrome()
+def _parse_date_now(tzone='US/Eastern'):
+    """
+    Parse default date now in Eastern Time. Basketball reference website is using US/Eastern timezone
 
-# Player stats
-def player_stats(year):
-    player_stats_url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html"
+    :param tzone: target timezone. The default is 'US/Eastern'.
+    :return: a date as now in Eastern Time.
+    """
 
-    url = player_stats_url.format(year)
-    
+    # Create a timezone-aware object for UTC
+    utc_timezone = pytz.utc
+
+    # Get the current date and time in UTC using timezone-aware objects
+    utc_now = datetime.now(tz=utc_timezone)
+
+    # Specify the target timezone as Eastern Time Zone (ET)
+    target_timezone = pytz.timezone(tzone)
+
+    # Convert the UTC date to the Eastern Time Zone
+    date_now = utc_now.astimezone(target_timezone).date()
+
+    return date_now
+
+def _convert_date_to_str(date_now):
+    """
+    Convert date to string; month, day, and year
+
+    :param date_now: a date as now.
+    :return: month, day, and year as US format MMDDYYYY
+    """
+    # Convert the Eastern Time Zone to month, day, and year
+    month_now = date_now.strftime("%m")
+    if month_now[0] == "0":
+        month_now = month_now[1]
+    day_now = date_now.strftime("%d")
+    year_now = date_now.strftime("%Y")
+
+    return month_now, day_now, year_now
+
+def _get_box_score_list():
+    """
+    Get a box score list in a match day.
+
+    :return: A list of html address of box score in a match day
+    """
+
+    # Convert the UTC date to the Eastern Time Zone
+    date_now = _parse_date_now()
+
+    # Convert the Eastern Time Zone to month, day, and year
+    month_now, day_now, year_now = _convert_date_to_str(date_now)
+
+    # Combine month, day and year to the url
+    day_match_url = "https://www.basketball-reference.com/boxscores/index.fcgi?month={month}&day={day}&year={year}"
+    url = day_match_url.format(month=month_now, day=day_now, year=year_now)
+
+    # Get the page using Chrome WebDriver to get html elements
     driver = webdriver.Chrome()
     driver.get(url)
     driver.execute_script("window.scrollTo(1,10000)")
     time.sleep(2)
-
-    dfs = []
     page = driver.page_source
+
+    # Using BeautifulSoup to parse the html of each match i.e. '/boxscores/202403180BOS.html'
     soup = BeautifulSoup(page, 'html.parser')
-    soup.find('tr', class_="thead").decompose()
-    player_table = soup.find_all(id="per_game_stats")[0]
-    player_df = pd.read_html(str(player_table))[0]
-    player_df["Year"] = year
-    dfs.append(player_df)
+    box_score_link = soup.find_all("a", string="Box Score")
+    box_score_list = []
+    for link in box_score_link:
+        box_score_list.append(link.get('href'))
 
-    players = pd.concat(dfs)
-
-    return players
+    return box_score_list
